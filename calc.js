00157af21b32e3b23f14c74d1040f7b14d8838ef
AadilBR <aadil.iphone@gmail.com>
2021-03-12 12:58:23 +0100

    first commit

---
+++ calc.js
@@ -0,0 +1,3 @@
+const { calc } = require('./calc')
+
+console.log(calc.calc('*', 10, 10))
---
+++ main.js
@@ -0,0 +1,10 @@
+const op = require('./operations') // ".js" optionnel à la fin du nom du module
+
+
+
+
+
+let nb1 = op.add(10, 11)
+console.log(nb1) // output: 21
+let nb2 = op.mul(5, 3)
+console.log(nb2) // output: 15
